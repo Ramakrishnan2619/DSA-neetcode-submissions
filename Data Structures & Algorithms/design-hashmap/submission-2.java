a031acabae8cf8acdb45d8ec2dd780211065984c
Ramakrishnan S <sramakrishnan2196@gmail.com>
2026-09-24 22:47:59 +0530

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.java
@@ -0,0 +1,21 @@
+class MyHashMap {
+    private int[] map;
+
+    public MyHashMap() {
+        map = new int[1000001];
+        // Initialize all values to -1 to signify they don't exist
+        java.util.Arrays.fill(map, -1);
+    }
+    
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
